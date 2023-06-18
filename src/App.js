fe673fdbbad099b265f17b1cda1d89021a4d3bfd
pyother <piotr.dominik.sobol@gmail.com>
2023-06-18 16:24:11 +0200

    Small deployment corrections

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home.jsx';
 
 function App() {
-  
   return(
     <Router>
       <Routes>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,9 @@
 import React from "react";
-import { Container, Grid } from "@mui/material";
 
 const Home = () => {
   return (
     <div>
-      <Container>
-        <Grid>
-          <h1>Map visualization</h1>
-        </Grid>
-      </Container>
+      
     </div>
   );
 };
